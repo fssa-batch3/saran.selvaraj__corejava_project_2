d8aef50ea181e399e92c3dc8857c1029f9c7608c
SARAN2004 <116723092+SARAN2004@users.noreply.github.com>
2023-08-04 15:47:15 +0530

    properties

--- src/main/java/petMall/App.java
+++ src/main/java/petMall/App.java
@@ -1,6 +1,6 @@
 package petMall;
 
-import java.sql.Connection;
+//import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -9,6 +9,9 @@ import petMall.model.*;
 
 public class App {
 	
+public static void main(String[] args) {
+	System.out.println("hiejhfuegf");
+}
 
 
 }
